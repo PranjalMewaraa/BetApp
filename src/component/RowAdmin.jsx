5e602ced2fda6acb30978f68afdf21689d1aba93
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2024-06-06 21:24:24 +0530

    /

--- src/component/RowAdmin.jsx
+++ src/component/RowAdmin.jsx
@@ -47,28 +47,7 @@ const RowAdmin = (props) => {
     <div className="w-full h-20 flex">
       <div className="w-full h-full bg-gray-900 bg-opacity-44 backdrop-blur-md border flex border-gray-900 border-opacity-45 rounded-lg shadow-lg">
         <div className="w-1/4 flex items-center h-full px-4">{props.name}</div>
-        <div className="w-1/4 flex items-center h-full px-4">
-          {`Rs${props.price}`}
-        </div>
-        <div
-          className="w-1/4 flex items-center h-full px-4 text-green-500"
-          style={{
-            color: props.rise
-              ? "rgb(34 197 94 / var(--tw-text-opacity))"
-              : "#DC2626",
-          }}
-        >
-          {props.change}
-          <span
-            style={{
-              color: props.rise
-                ? "rgb(34 197 94 / var(--tw-text-opacity))"
-                : "#DC2626",
-            }}
-          >
-            {props.rise ? "↗" : "↘"}
-          </span>
-        </div>
+
         <div className="w-1/4 flex items-center h-full px-4 gap-2 flex justify-end">
           <label className="inline-flex items-center cursor-pointer">
             <input
