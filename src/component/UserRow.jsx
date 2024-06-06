07c8a4d8a7391db9a88c0d70bf9d2acb8a3dd31d
PranjalMewaraa <102512201+PranjalMewaraa@users.noreply.github.com>
2024-06-06 12:57:17 +0530

    ..

--- src/component/UserRow.jsx
+++ src/component/UserRow.jsx
@@ -2,13 +2,15 @@
 
 function UserRow(props) {
   return (
-    <div className="w-full h-20 flex flex-wrap justify-evenly">
-      <div className="w-full h-full bg-gray-900 bg-opacity-44 backdrop-blur-md border flex flex-wrap border-gray-900 border-opacity-45 rounded-lg shadow-lg ">
-        <div className="w-auto flex items-center  h-full px-4">{props.idx}</div>
+    <div className="w-full min-h-20 h-fit flex gap-8 flex-wrap justify-evenly">
+      <div className="w-full h-full p-8 bg-gray-900 bg-opacity-44 backdrop-blur-md border flex gap-4 flex-wrap text-white border-gray-900 border-opacity-45 rounded-lg shadow-lg ">
+        <div className="w-auto flex items-center text-white  h-full px-4">
+          {props.idx}
+        </div>
         {/* <div className="w-auto flex items-center  h-full px-4">
           {props.name}
         </div> */}
-        <div className="w-auto flex items-center  h-full px-4">
+        <div className="w-auto flex items-center text-white h-full px-4">
           {`${props.email}`}{" "}
         </div>
       </div>
